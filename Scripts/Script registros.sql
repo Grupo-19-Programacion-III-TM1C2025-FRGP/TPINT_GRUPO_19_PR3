88d660d66bcaf7db9a205cca8fb0387ae911ea91
H4RR1S-D3V <harrisdm.dev@gmail.com>
2025-06-19 12:43:08 -0300

    creacion de base de datos, tablas y registros

---
+++ Scripts/Script registros.sql
@@ -0,0 +1,81 @@
+USE Clinica
+GO
+
+INSERT INTO Especialidades (CodEspecialidad_Es, NombreEspecialidad_Es)
+SELECT 1, 'Kinesiología'
+UNION SELECT 2, 'Traumatología'
+UNION SELECT 3, 'Cardiología'
+UNION SELECT 4, 'Clínica Médica'
+UNION SELECT 5, 'Pediatría'
+UNION SELECT 6, 'Dermatología'
+UNION SELECT 7, 'Ginecología'
+UNION SELECT 8, 'Neurología'
+UNION SELECT 9, 'Urología'
+UNION SELECT 10, 'Oncología'
+UNION SELECT 11, 'Psiquiatría'
+UNION SELECT 12, 'Traumatología'
+UNION SELECT 13, 'Reumatología'
+UNION SELECT 14, 'Endocrinología'
+UNION SELECT 15, 'Infectología'
+UNION SELECT 16, 'Neumonología'
+UNION SELECT 17, 'Hematología'
+UNION SELECT 18, 'Fonoaudiología'
+UNION SELECT 19, 'Nutrición'
+GO
+
+INSERT INTO DiasAtencion (CodDiaAtencion_DA, NombreDia_DA)
+SELECT 1, 'Lunes'
+UNION SELECT 2, 'Martes'
+UNION SELECT 3, 'Miércoles'
+UNION SELECT 4, 'Jueves'
+UNION SELECT 5, 'Viernes'
+UNION SELECT 6, 'Sábado'
+UNION SELECT 7, 'Domingo'
+GO
+
+INSERT INTO HorariosAtencion (CodHorarioAtencion_HA, HoraInicio_HA, HoraFin_HA)
+SELECT 1, '07:00', '08:00'
+UNION SELECT 2, '08:00', '09:00'
+UNION SELECT 3, '09:00', '10:00'
+UNION SELECT 4, '10:00', '11:00'
+UNION SELECT 5, '11:00', '12:00'
+UNION SELECT 6, '12:00', '13:00'
+GO
+
+INSERT INTO HorariosDeAtencionPorDia (CodDiaAtencion_HAD, CodHorarioAtencion_HAD)
+-- Lunes de 7 a 13
+SELECT 1, 1
+UNION SELECT 1, 2
+UNION SELECT 1, 3
+UNION SELECT 1, 4
+UNION SELECT 1, 5
+UNION SELECT 1, 6
+-- Martes de 7 a 13
+UNION SELECT 2, 1
+UNION SELECT 2, 2
+UNION SELECT 2, 3
+UNION SELECT 2, 4
+UNION SELECT 2, 5
+UNION SELECT 2, 6
+-- Miércoles de 7 a 13
+UNION SELECT 3, 1
+UNION SELECT 3, 2
+UNION SELECT 3, 3
+UNION SELECT 3, 4
+UNION SELECT 3, 5
+UNION SELECT 3, 6
+-- Jueves de 7 a 13
+UNION SELECT 4, 1
+UNION SELECT 4, 2
+UNION SELECT 4, 3
+UNION SELECT 4, 4
+UNION SELECT 4, 5
+UNION SELECT 4, 6
+-- Viernes de 7 a 13
+UNION SELECT 5, 1
+UNION SELECT 5, 2
+UNION SELECT 5, 3
+UNION SELECT 5, 4
+UNION SELECT 5, 5
+UNION SELECT 5, 6
+GO
