4d62839a12c90e5bd8de5f8f78a7ec1138ce4dc6
H4RR1S-D3V <harrisdm.dev@gmail.com>
2025-06-20 18:20:10 -0300

    metodos traer tabla (esp, dia, hor)

---
+++ Datos/DaoHorarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DaoHorarios : GenericDao
+    {
+        readonly string consultaTabla = "SELECT * FROM HorariosAtencion";
+
+        public DaoHorarios() { }
+
+        public DataTable getTablaHorarios()
+        {
+            DataTable tabla = _conexion.TraerTabla(consultaTabla, "HorariosAtencion");
+            return tabla;
+        }
+    }
+}
